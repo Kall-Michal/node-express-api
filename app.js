653d0757611af717f79a22d4cde2c12b6c76772b
Malcolm Kente <malcolm.kente@gmail.com>
2019-05-09 16:41:40 +0200

    Modified project structure

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const libRoutes = require("./routes/library-routes");
+const libController = require("./controllers/library-routes");
 const app = express();
 const port = 3000;
 
@@ -7,22 +7,22 @@ const port = 3000;
 app.use(express.json());
 
 // GET - uses the logic passed in read found inside the module library-routes.
-app.get("/api/books", libRoutes.read);
+app.get("/api/books", libController.read);
 
 // GET - uses - query
-app.get("/api/books/search?", libRoutes.query);
+app.get("/api/books/search?", libController.query);
 
 // POST - uses the logic passed in create found inside the module library-routes.
-app.post("/api/books", libRoutes.create);
+app.post("/api/books", libController.create);
 
 // PUT - uses the logic passed in update found inside the module library-routes.
-app.put("/api/books/:bookId", libRoutes.update);
+app.put("/api/books/:bookId", libController.update);
 
 // DELETE - uses the logic passed in delete found inside the module library-routes.
-app.delete("/api/books/:bookId", libRoutes.delete);
+app.delete("/api/books/:bookId", libController.delete);
 
 // GET - uses - query
-app.get("/api/books/search?", libRoutes.query);
+app.get("/api/books/search?", libController.query);
 
 // Listen to app
 app.listen(port, () => console.log(`Listening to port ${port}`));
